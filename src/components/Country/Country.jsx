b85e3c02daf150d51d59b360ccf9c14aad32979b
mdalrasel <mdalrasel.dev@gmail.com>
2025-03-30 08:37:31 +0600

    first commit

---
+++ src/components/Country/Country.jsx
@@ -0,0 +1,13 @@
+
+
+const Country = ({country}) => {
+    console.log(country)
+    return (
+        <div>
+            <h3>Name : {country.name.common}</h3>
+            
+        </div>
+    );
+};
+
+export default Country;
